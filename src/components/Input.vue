e4886fe398e9467f57a521f9636824d03e4a3ce5
Hromsi <bodak2011@yandex.ru>
2024-02-22 03:45:16 +0300

    init

---
+++ src/components/Input.vue
@@ -0,0 +1,21 @@
+<template>
+    <input type="number" placeholder="Введите число"/>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+input {
+    outline: none;
+    width: 500px;
+    position: relative;
+    top: -50px;
+    font-size: 1.2em;
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+}
+</style>
